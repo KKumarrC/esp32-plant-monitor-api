ed9870bc298b60d967944dee7cc82b5d5a8afa80
Kumar Chipepo <154108516+KKumarrC@users.noreply.github.com>
2026-01-13 15:00:48 -0500

    updated api url from local to cloud

--- hardware/secrets.h
+++ hardware/secrets.h
@@ -1,4 +1,4 @@
 #pragma once
 #define WIFI_SSID "WeLoveTmobile2"
 #define WIFI_PASS "$$Kumar814!!@1231&164"
-#define API_URL   "http://192.168.12.178:5000/readings"
+#define API_URL   "https://plant-monitor-api-dc28347b13d8.herokuapp.com/readings"
